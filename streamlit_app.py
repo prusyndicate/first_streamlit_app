c3ac15c8ae4f1f5a0990a46f1ce091733ab3eff1
prusyndicate <146290993+prusyndicate@users.noreply.github.com>
2023-09-28 12:10:45 +0100

    Update streamlit_app.py
    added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents first dinner')
-streamlit.header('Blueberry Oatmeal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Omega3')
+streamlit.text('Kale')
+streamlit.text('Boiled Egg')
+streamlit.text('Smoothie')
