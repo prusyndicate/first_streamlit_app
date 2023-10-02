1f8dd7255b6271537fa751b0a8234b27bb4452d0
prusyndicate <146290993+prusyndicate@users.noreply.github.com>
2023-10-02 13:54:54 +0100

    Update streamlit_app.py
    j

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,5 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/orange")
 streamlit.text(fruityvice_response)
-streamlit.header("Fruityvice Fruit Advice!")
